9288ac9911b82dff7e3d55f2581306282c9844bf
stlp <jeyoung1159@gmail.com>
2020-10-17 16:42:37 -0700

    Canvas api test set up/Library/Developer/CommandLineTools/usr/bin/python3

--- api-test.py
+++ api-test.py
@@ -6,8 +6,8 @@ API_KEY = "10~xLbBIb7Mw9HOU5ce3ihhXTl2A8nLHqEbppHxfZnsGO2zda3IwtES54Sx1qStOHwy"
 
 canvas = Canvas(API_URL, API_KEY)
 user = canvas.get_user("self")
-user_calendar = user.get_calendar_events_for_user()
+user_calendar = user.get_courses
 
-for calendar in user_calendar:
-    print(calendar)
+print(user_calendar)
+
 #10~xLbBIb7Mw9HOU5ce3ihhXTl2A8nLHqEbppHxfZnsGO2zda3IwtES54Sx1qStOHwy
