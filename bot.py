27e6b51b7fef3fba7b2088e0e1e9044bc0ed3431
stlp <jeyoung1159@gmail.com>
2020-10-17 20:47:25 -0700

    Add showCourses and showEvents

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
+from canvas import getCourses, getEvents
+import discord
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -23,17 +25,34 @@ async def david(ctx):
 async def createrole(ctx, role: str):
   await ctx.guild.create_role(name=role)
 
+@bot.command(name='showCourse', help="Show the courses that you are in")
+async def showcourse(ctx):
+  courseList = getCourses()
+  res = ""
+  for course in courseList:
+    res += "- " + course + "\n"
+  embed = discord.Embed()
+  embed.add_field(name="Current Courses", value=res, inline=False)
+  await ctx.send(embed=embed)
+
+@bot.command(name='events', help="Shows the upcoming events. <input> = OH or Section")
+async def showEvent(ctx, input: str):
+  eventList = getEvents()
+  res = ""
+  if (input.lower() == "oh"):
+    for event in eventList:
+      if ('OH' in event['name'] or 'Office Hours' in event['name']):
+        res += "- " + event['name'] + " " + event['start_time'] + "\n"
+  embed = discord.Embed()
+  embed.add_field(name="Upcoming Events", value=res, inline=False)
+  await ctx.send(embed=embed)
+
 # Incorrect role error message
 @bot.event
 async def on_command_error(ctx, error):
   if isinstance(error, commands.errors.CheckFailure):
     await ctx.send('You do not have the correct role for this command.')
-
-# Incorrect command error
-@bot.event
-async def on_command_error(ctx, error):
-  if isinstance(error, commands.CommandNotFound):
+  elif isinstance(error, commands.CommandNotFound):
     await ctx.send('This command does not exist. Please use c!help to see valid commands.')
-
 
 bot.run(TOKEN)
---
+++ canvas.py
@@ -0,0 +1,23 @@
+import requests
+
+API_URL = "https://canvas.uw.edu"
+API_KEY = "10~xLbBIb7Mw9HOU5ce3ihhXTl2A8nLHqEbppHxfZnsGO2zda3IwtES54Sx1qStOHwy"
+headers = {'Authorization': 'Bearer %s' % API_KEY}
+
+def getEvents():
+    upcoming_events = requests.get("https://canvas.uw.edu/api/v1/users/self/upcoming_events", headers=headers).json()
+    event = []
+    for events in upcoming_events:
+        event.append({
+            'name': events['title'],
+            'start_time': events['start_at']
+        })
+    return event
+
+def getCourses():
+    fav_courses = requests.get("https://canvas.uw.edu/api/v1/courses", headers=headers, params={'enrollment_state':'active', 'include':['term']}).json()
+    result = []
+    for course in fav_courses:
+        if (course['term']['name'] == "Autumn 2020"):
+            result.append(course['name'])
+    return result
