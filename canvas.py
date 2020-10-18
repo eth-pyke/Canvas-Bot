96a9b2c4a4a38ab8be2b284b66ff63591a6c0639
BuildTools <unconfigured@null.spigotmc.org>
2020-10-18 01:34:29 -0700

    Added more database implementation

--- canvas.py
+++ canvas.py
@@ -1,8 +1,9 @@
 import requests
+import os
 
 API_URL = "https://canvas.uw.edu"
-API_KEY = "10~xLbBIb7Mw9HOU5ce3ihhXTl2A8nLHqEbppHxfZnsGO2zda3IwtES54Sx1qStOHwy"
-headers = {'Authorization': 'Bearer %s' % API_KEY}
+KEY = os.getenv('API_KEY')
+headers = {'Authorization': 'Bearer %s' % KEY}
 
 def getEvents():
     upcoming_events = requests.get("https://canvas.uw.edu/api/v1/users/self/upcoming_events", headers=headers).json()
